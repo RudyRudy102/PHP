2ecd063fb52cc8202b86394a7026cf3a7f60e817
Patryk Brochocki <rudyjestrudy0000@gmail.com>
2025-04-03 14:55:15 +0200

    Create Zajęcia1.php

---
+++ Zajęcia1.php
@@ -0,0 +1,19 @@
+ssh s24@193.0.78.160
+s24.ABCD
+cd public_html
+mkdir Papryk
+cd Papryk
+nano index.php
+
+cd public_html/Papryk
+http://193.0.78.160/~s24/Papryk
+
+
+<html lang="pl">
+<head>
+ <meta charset="utf-8">
+</head>
+<body>
+PB Start
+</body>
+</html>
